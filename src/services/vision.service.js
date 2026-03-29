abe491badb8faf4a4ab70be3af9c8f5529850c97
safeer <safeersifarath@gmail.com>
2026-03-29 12:40:48 +0530

    improvement 8

--- src/services/vision.service.js
+++ src/services/vision.service.js
@@ -6,7 +6,7 @@ const makeVisionRequest = async (imageBase64) => {
   return axios.post(
     "https://api.groq.com/openai/v1/chat/completions",
     {
-      model: "llama-3.2-11b-vision-preview",
+      model: "meta-llama/llama-4-scout-17b-16e-instruct",
       messages: [
         {
           role: "system",
